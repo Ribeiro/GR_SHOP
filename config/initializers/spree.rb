49fe6a59caad2e16f67a6c07b57c717257ac330e
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2015-08-19 12:19:56 -0300

    first commit

---
+++ config/initializers/spree.rb
@@ -0,0 +1,21 @@
+# Configure Spree Preferences
+#
+# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
+#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
+#
+# Note: If a preference is set here it will be stored within the cache & database upon initialization.
+#       Just removing an entry from this initializer will not make the preference value go away.
+#       Instead you must either set a new value or remove entry, clear cache, and remove database entry.
+#
+# In order to initialize a setting do:
+# config.setting_name = 'new value'
+Spree.config do |config|
+  # Example:
+  # Uncomment to stop tracking inventory levels in the application
+  # config.track_inventory_levels = false
+end
+
+Spree.user_class = "Spree::User"
+SpreeI18n::Config.available_locales = [:en, :'pt-BR'] # displayed on translation forms
+SpreeI18n::Config.supported_locales = [:en, :'pt-BR'] # displayed on frontend select box
+#I18n.enforce_available_locales = false
